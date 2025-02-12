1de858efa2c0e38a6b19e269186acecfb4675339
reyan mumtaz <reyanmumtaz75@gmail.com>
2025-02-12 16:02:32 +0500

    Create lines.py

---
+++ 01_python_fundamental/01_Lines_and_Indentation/lines.py
@@ -0,0 +1,7 @@
+# ✅ Use line breaks for clarity
+if (value > 10 and value < 20 and 
+    value != 15):
+    print("Value is valid!")
+
+# ❌ Everything on one line is difficult to read
+if (value > 10 and value < 20 and value != 15): print("Value is valid!")
